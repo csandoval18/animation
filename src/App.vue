a2c6207f5a1ab82720c1a2681f70ee2148216677
csandoval18 <christian_alvites@outlook.com>
2022-09-08 21:28:35 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+	hello world <a href="">hey this is a link</a>
+	<button>button</button>
+</template>
+
+<style scoped></style>
+<!-- 
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>hello world</template>
+
+<style scoped>
+.read-the-docs {
+	color: #888;
+}
+</style> -->
