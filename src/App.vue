aae2e6491acfb65b04c8cac9404d01a563e1831d
csandoval18 <christian_alvites@outlook.com>
2022-09-09 18:18:00 -0500

    created box animation

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,70 @@
 </script>
 
 <template>
-	hello world <a href="">hey this is a link</a>
-	<button>button</button>
+	<div class="app-container">
+		<header class="title">CSS Animations</header>
+		<div class="animation-container">
+			<div class="box"></div>
+		</div>
+	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.title {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-weight: 700;
+	height: 8rem;
+	font-size: 3rem;
+	color: white;
+}
+
+.animation-container {
+	display: flex;
+	padding-left: 38%;
+}
+
+.box {
+	width: 100px;
+	height: 100px;
+	background-color: red;
+	position: relative;
+	animation-name: square;
+	animation-duration: 2s;
+}
+
+@keyframes square {
+	0% {
+		background-color: red;
+		left: 0;
+		top: 0;
+	}
+	25% {
+		background-color: gold;
+		left: 30%;
+		top: 0px;
+	}
+	50% {
+		background-color: lime;
+		left: 30%;
+		top: 500px;
+		border-radius: 50%;
+	}
+	70% {
+		background-color: dodgerblue;
+		left: 0px;
+		top: 500px;
+	}
+	100% {
+		background-color: purple;
+		left: 0px;
+		top: 0px;
+		border-radius: 0px;
+	}
+}
+</style>
+
 <!-- 
 <script setup lang="ts">
 import { ref } from 'vue'
